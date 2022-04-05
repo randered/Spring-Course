b36791f1b7ac5c35b340770c72d8ef706bb4c7de
randered <68559794+randered@users.noreply.github.com>
2022-04-05 13:32:10 +0300

    Trying new Spring Project with MongoDB

--- spring5-recipe-app/src/test/java/guru/springframework/services/RecipeServiceImplTest.java
+++ spring5-recipe-app/src/test/java/guru/springframework/services/RecipeServiceImplTest.java
@@ -1,10 +1,8 @@
 package guru.springframework.services;
 
-import guru.springframework.domain.Recipe;
 import guru.springframework.repositories.RecipeRepository;
 import junit.framework.TestCase;
 import org.junit.Before;
-import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
@@ -23,16 +21,4 @@ public class RecipeServiceImplTest extends TestCase {
         recipeService = new RecipeServiceImpl(recipeRepository);
     }
 
-    @Test
-    public void testGetRecipes() throws Exception{
-        Recipe recipe = new Recipe();
-        HashSet recipeData = new HashSet();
-        recipeData.add(recipe);
-
-        when(recipeRepository.findAll()).thenReturn(recipeData);
-        Set<Recipe> recipes = recipeService.getRecipes();
-
-        assertEquals(recipes.size(), 1);
-        verify(recipeRepository, times(1)).findAll();
-    }
 }
